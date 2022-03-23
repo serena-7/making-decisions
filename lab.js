bc6b2fc488b2acda85d199a4d3ed1d125719a0bc
Serena Sorensen <serena.sorensen7@gmail.com>
2022-03-23 15:48:19 -0600

    all problems complete

--- lab.js
+++ lab.js
@@ -61,19 +61,19 @@ let rain = true;
 
 //CODE HERE
 if (temperature >= 80){
-  if (rain = true) {
+  if (rain) {
     console.log("wear a t-shirt and take an umbrella");
   } else {
     console.log("wear a t-shirt");
   }
 } else if (temperature > 60 && temperature < 80){
-  if (rain = true) {
+  if (rain) {
     console.log("wear a rain jacket");
   } else {
     console.log("wear a light jacket");
   }
 } else if (temperature <= 60){
-  if (rain = true) {
+  if (rain) {
     console.log("wear a jacket and carry an umbrella")
   } else {
     console.log("wear a coat");
@@ -138,11 +138,19 @@ while(score < passingScore){
 */
 
 //CODE HERE
+let changeMyMind = true;
+if (changeMyMind = true){
+  changeMyMind = false;
+} else {
+  changeMyMind = true;
+}
 
 ////////// PROBLEM 10 //////////
 // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
 
 //CODE HERE
+changeMyMind = !changeMyMind;
+console.log(changeMyMind);
 
 ////////// ADVANCED PROBLEMS //////////
 
@@ -152,3 +160,9 @@ let z = 5;
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 //CODE HERE
+while(z > 0){
+  for(i = z; i > 0; i--){
+    console.log(i);
+  }
+  z--;
+}
